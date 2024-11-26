ac158ed622ed8dbacdf525f0d2c39c4114c11343
EundaChung <22200680@handong.ac.kr>
2024-11-26 15:46:57 +0900

    DefaultController - index.html, test.html 테스트

--- src/main/java/com/example/capstone1/controller/DefaultController.java
+++ src/main/java/com/example/capstone1/controller/DefaultController.java
@@ -3,14 +3,16 @@ package com.example.capstone1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
-@RestController
+@RequestMapping("")
+@Controller
 public class DefaultController {
-    @GetMapping("/index")
-    public String index(){
-        return "test test test";
+    @GetMapping({"/index"})
+    public String index() {
+        return "index";
     }
-
+    @GetMapping({"/test"})
+    public String test() {
+        return "test";
+    }
 }
